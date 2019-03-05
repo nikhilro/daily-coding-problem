bb930b78faa41208464238ba99dbccb2fc63ce13
Nikhil Gupta <niks.gupta09@gmail.com>
2019-03-05 15:58:24 +0900

    #23 bfs

--- p23.py
+++ p23.py
@@ -1,4 +1,5 @@
 import math
+import queue
 
 steps = {}
 path = set()
@@ -37,3 +38,37 @@ board = [[False, False, False, False],
         [False, False, False, False]]
 
 print(min_steps(board, (3, 0), (0, 0)))
+
+def min_steps_2(board, start, end): # bfs, O(m * n) time, O(m * n) space
+    cache = set()
+    q = queue.Queue()
+    q.put((start, 0))
+
+    def validate(move, steps):
+        if move[0] < len(board) and move[0] >= 0 and \
+                move[1] < len(board[0]) and move[1] >= 0 and \
+                not board[move[0]][move[1]] and \
+                move not in cache:  # not a wall or prev position
+            return q.put((move, steps + 1))
+        
+    while not q.empty():
+        start, steps = q.get(block=False)
+        cache.add(start)
+
+        if start == end:
+            return steps
+        
+        validate((start[0], start[1] + 1), steps)  # right
+        validate((start[0] + 1, start[1]), steps)  # down
+        validate((start[0], start[1] - 1), steps)  # left
+        validate((start[0] - 1, start[1]), steps)  # up
+
+    return None 
+
+print(min_steps_2(board, (3, 0), (0, 0)))
+
+
+
+        
+
+
