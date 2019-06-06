3c5e75cc1b39b4ba1d652db6b4439837d88ccbd3
Nikhil Gupta <niks.gupta09@gmail.com>
2019-06-06 07:05:15 -0400

    #76

---
+++ p76.py
@@ -0,0 +1,17 @@
+def remove_columns(matrix):
+    count = 0
+    for j in range(len(matrix[0])):
+        for i in range(len(matrix)):
+            if i > 0 and matrix[i - 1][j] > matrix[i][j]:
+                count += 1
+                break
+    return count
+
+matrix = ['cba', 'daf', 'ghi']
+print(remove_columns(matrix))
+
+matrix = ['abcdef']
+print(remove_columns(matrix))
+
+matrix = ['zyx', 'wvu', 'tsr']
+print(remove_columns(matrix))
