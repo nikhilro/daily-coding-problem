6b63c88bc0f1fa656fc2b1c96b476535af493482
Nikhil Gupta <niks.gupta09@gmail.com>
2019-06-08 06:33:58 -0400

    #79

---
+++ p79.py
@@ -0,0 +1,13 @@
+def check_non_decreasing(arr):
+    count = 0
+    for i in range(1, len(arr)):
+        if arr[i] < arr[i - 1]:
+            if count:
+                return False
+            if not (i == len(arr) - 1 or i == 1
+                        or arr[i - 1] <= arr[i + 1] or arr[i] >= arr[i - 2]):
+                return False
+            count += 1
+    return True
+
+print(check_non_decreasing([10, 5, 7]), check_non_decreasing([10, 5, 1]))
